69a1058c8c5133cc33985f365d8747653ef4917e
AnuSandhu <sandhu@Sandhus-MacBook-Air.local>
2015-09-30 17:42:53 -0700

    adding finding object file

---
+++ finding_lost_object.py
@@ -0,0 +1,15 @@
+# introducing how items will be found via numerology.
+print ("Finding lost objects with the help of numbers is an old and proven method of tapping into your subconscious.  As with other aspects of life where the relationship between cause and effect is obscured, doubts have no effect on the reality of what takes place.  Hence, if you find it difficult to create a nine-digit number, perhaps because you feel too self-conscious, changing your mind ten times in the process, remember that no matter how you reach the final number, it is the number that tells all.  Period.")
+print 'I am here to help you.'
+# interaction with client, asks for name.
+# questions if item lost?
+ #asks person if they lost an object.
+name = raw_input('Tell me your name.')
+number = []
+sum = 0
+# want to check if numbers added are greater then 6 and there are 9 digits in the response
+while True and (sum < 6 and len(numbers != 9)):
+    if (response == 'yes'  or response == 'no'):
+        break
+    else:
+        print('Please answer "yes" or "no".')
